74434ca1a165a442fe825834aa3c58275fc83832
NaveenR <naveenrvinayaka@gmail.com>
2018-08-03 16:00:06 +0530

    Added comments

--- src/main/scala/com/navecom/logs/utils/LogUtils.scala
+++ src/main/scala/com/navecom/logs/utils/LogUtils.scala
@@ -39,18 +39,25 @@ object LogUtils extends ProjectConfig {
     }).reduceByKey(_+_)
   }
 
-  // user who visited sports department more than 10 times
-  def departmentsCount1(accessLogs: RDD[AccessLog]) = {
+  /*
+  This function takes accessLogs and  number 'n'
+  and returns the users who all visited sports department more than n times
+  */
+
+  def sportsUserVisits(accessLogs: RDD[AccessLog],n:Int) = {
     val sportdept = accessLogs.map(x => (x.clientIdentd, x.endpoint)).//filter(x => x._2.split("/")(1)=="sport")
       filter(x => {
       val endpoint = x._2
       val d = endpoint.split("/")
-      (d(1) == "sport")
-    }).map(x => (x, 1)).reduceByKey(_+_).filter(x => x._2.toInt>10)
+      d(1) == "sport"
+    }).map(x => (x, 1)).reduceByKey(_+_).filter(x => x._2.toInt>n)
 
   }
 
-//total-login from each user
+  /*
+  This function takes accessLogs as input and
+  returns total logins of each user
+  */
   def LoginCountFromEachUser(accessLogs: RDD[AccessLog]) = {
     val requestIniated =accessLogs.map(x => (x.clientIdentd,x.endpoint)).
       filter(x => x._2.split("/")(0)=="log-in").
@@ -59,7 +66,11 @@ object LogUtils extends ProjectConfig {
       map(x => (x._1._1,x._2))
   }
 
-  //how many add to cart from each user
+
+  /*
+  This function takes accessLogs as input and
+  returns total add-to-cart from each user
+  */
   def addToCart(accessLogs: RDD[AccessLog]) = {
     val requestIniated =accessLogs.map(x => (x.clientIdentd,x.endpoint)).
       filter(x => x._2.split("/")(0)=="add-to-cat").
@@ -68,7 +79,10 @@ object LogUtils extends ProjectConfig {
       map(x => (x._1._1,x._2))
   }
 
-  //calls from mac
+  /*
+  This function takes accessLogs as input and
+  returns total calls from the Mac book
+  */
   def callFromMac(accessLogs: RDD[AccessLog]) = {
     val requestIniated =accessLogs.map(x => x.browser).
       filter(x => x.split(" ")(0)=="(Macintosh").
@@ -76,6 +90,11 @@ object LogUtils extends ProjectConfig {
       reduceByKey(_+_).
       map(x => ("MacCalls",x._2))
   }
+
+  /*
+  This function takes accessLogs as input and
+  returns total calls from the windows
+  */
 
   def callFromWindows(accessLogs: RDD[AccessLog]) = {
     val requestIniated =accessLogs.map(x => x.browser).
@@ -85,6 +104,10 @@ object LogUtils extends ProjectConfig {
       map(x => ("MacCalls",x._2))
   }
 
+  /*
+  This function takes accessLogs as input and
+  returns total calls from the Linux OS
+  */
   def callFromLinux(accessLogs: RDD[AccessLog]) = {
     val requestIniated =accessLogs.map(x => x.browser).
       filter(x => x.split(" ")(0)=="(Linux").
@@ -93,27 +116,35 @@ object LogUtils extends ProjectConfig {
       map(x => ("MacCalls",x._2))
   }
 
-  // what are the responsecode and how many ttimes they were returned
-
-  def reasonCodes(accessLogs: RDD[AccessLog]) = {
+  /*
+  This function takes accessLogs as input and
+  returns responseCodes and thier counts
+  */
+  def responseCodeCounts(accessLogs: RDD[AccessLog]) = {
     val requestIniated =accessLogs.map(x => (x.responseCode,1)).
       reduceByKey(_+_)
   }
 
-  // how much bytes were transferred
-
+  /*
+  This function takes accessLogs as input and
+  returns total bytes transferred from the website
+  */
   def bytestransfreed(accessLogs: RDD[AccessLog]) = {
     val requestIniated =accessLogs.map(x => x.contentSize).
       reduce(_+_)
   }
 
-  // 404 url
+  /*
+  This function takes accessLogs as input and
+  returns all the URLs showed 404 */
   def url404(accessLogs: RDD[AccessLog]) = {
     val requestIniated =accessLogs.map(x => (x.endpoint,x.responseCode)).filter(x => x._2.toInt==404).map(x=>x._1)
   }
 
-
-  //how many check-out from each user
+  /*
+  This function takes accessLogs as input and
+  returns total number of checkouts from each user
+  */
   def checkedout(accessLogs: RDD[AccessLog]) = {
     val requestIniated =accessLogs.map(x => (x.clientIdentd,x.endpoint)).
       filter(x => x._2.split("/")(0)=="check-out").
@@ -122,7 +153,7 @@ object LogUtils extends ProjectConfig {
       map(x => (x._1._1,x._2))
   }
 
-  //checked out everyday
+  //checked out everyday - To Do
   def checkedoute(accessLogs: RDD[AccessLog]) = {
     val requestIniated =accessLogs.map(x => (x.clientIdentd,x.endpoint)).
       filter(x => x._2.split("/")(0)=="check-out").
@@ -131,22 +162,31 @@ object LogUtils extends ProjectConfig {
       map(x => (x._1._1,x._2))
   }
 
-  // user who visited sports department more than 10 times
-  def golf(accessLogs: RDD[AccessLog]) = {
+  /*
+  This function takes accessLogs
+  and returns the users who all visited golf department
+  */
+
+  def golf(accessLogs: RDD[AccessLog],n:Int) = {
     val sportdept = accessLogs.map(x => (x.clientIdentd, x.endpoint)).//filter(x => x._2.split("/")(1)=="sport")
       filter(x => {
       val endpoint = x._2
       val d = endpoint.split("/")
-      (d(1) == "golf")
+      d(1) == "golf"
     }).map(x => x._1)
   }
-  // user who visited sports department more than 10 times
-  def fitness(accessLogs: RDD[AccessLog]) = {
+
+
+  /*
+  This function takes accessLogs
+  and returns the users who all visited fitness department
+  */
+  def fitness(accessLogs: RDD[AccessLog],n:Int) = {
     val sportdept = accessLogs.map(x => (x.clientIdentd, x.endpoint)).//filter(x => x._2.split("/")(1)=="sport")
       filter(x => {
       val endpoint = x._2
       val d = endpoint.split("/")
-      (d(1) == "fitness")
+      d(1) == "fitness"
     }).map(x => x._1)
   }
 
